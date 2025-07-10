44c8c38e6abbe0ff4b0439ff9149c6f74d3b9d64
Nikhhel07 <nikhilnarine70@gmail.com>
2025-07-10 20:01:55 +0530

    Create Flask.py

---
+++ Flask.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_visitor_info():
+    visitor_ip = request.remote_addr
+    current_time = datetime.utcnow().isoformat()
+    return jsonify({
+        "timestamp": current_time,
+        "ip": visitor_ip
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
